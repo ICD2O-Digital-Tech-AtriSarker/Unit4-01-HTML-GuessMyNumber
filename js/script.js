2722b38291317f4e96baf9ca18b356fc7acc345a
Atri-Sarker <152197131+Atri-Sarker@users.noreply.github.com>
2024-04-21 18:10:52 +0000

    Main COmmit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,31 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
-// This file contains the JS functions for index.html
+
+// ELEMENTS
+const guessInput = document.getElementById('guessInput');
+const guessBtn = document.getElementById('GuessBtn');
+const outputResult = document.getElementById('outputResult');
+const guessForm = document.getElementById('guessForm');
+
+function guess() {
+
+  // Randomly Generated Number
+  let correctGuess = Math.floor((Math.random() * (6)) + 1);
+  // User input: Guessed Number 1-6
+  let guessedNumber = parseInt(guessInput.value);
+
+  // Check if guess matches
+  if (guessedNumber == correctGuess) {
+    outputResult.innerText = `The correct Number was ${correctGuess}, congratulations on guessing correctly!`
+    return;
+  }
+  // else { ... } [ Unsure whether else statements are allowed
+  // so I used returns instead ]
+  outputResult.innerText = `The correct Number was ${correctGuess}, you guessed ${guessedNumber}! Better Luck next time!`
+  return;
+}
+
+// Cool function notation
+// form validation + submit call
+guessForm.onsubmit = () => {
+  guess()
+  return false;
+}
